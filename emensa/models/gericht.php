aeb5d744484cbd3d550e411659f56cfdc228c53c
majd bousaad <bousaadmajd@gmail.com>
2022-11-30 04:23:17 +0100

    models for werbeseite
    zahlen + gerichte nehmen

--- emensa/models/gericht.php
+++ emensa/models/gericht.php
@@ -50,3 +50,76 @@ function db_gericht_select_names_preis_intern(){
         return $data;
     }
 }
+
+function zufaellige_gerichte(){
+    try {
+        $link = connectdb();
+
+        $sql = "SELECT name, 
+                               preis_intern, 
+                               preis_extern, 
+                               group_concat(code) as code
+                                    from gericht
+                             left join gericht_hat_allergen on gericht.id = gericht_hat_allergen.gericht_id
+                         group by gericht.name ORDER BY RAND() limit 5";
+        $result = mysqli_query($link, $sql);
+
+        $data = mysqli_fetch_all($result, MYSQLI_BOTH);
+
+        mysqli_close($link);
+    }
+    catch (Exception $ex) {
+        $data = array(
+            'id'=>'-1',
+            'error'=>true,
+            'name' => 'Datenbankfehler '.$ex->getCode(),
+            'beschreibung' => $ex->getMessage());
+    }
+    finally {
+        return $data;
+    }
+
+}
+
+function codes_from_zufaellige_gerichte($data) {
+
+    $codes = "";
+    foreach ($data as $gericht){
+        if($gericht['code'] != null){
+            if($codes != ""){
+                $codes.=',';
+
+            }
+            $codes .= $gericht['code'];
+        }
+    }
+    $codes = explode(",", $codes);
+    for ($i = 0; $i < count($codes); $i++)
+        $codes[$i] = "'" . $codes[$i] . "'";
+
+    // wieder als String sprichern
+    $codes = implode(",", $codes);
+
+    try {
+        $link = connectdb();
+
+        $sql = "SELECT code, name FROM allergen WHERE code IN ($codes)";
+        $result = mysqli_query($link, $sql);
+
+        $data = mysqli_fetch_all($result, MYSQLI_BOTH);
+
+        mysqli_close($link);
+    }
+    catch (Exception $ex) {
+        $data = array(
+            'id'=>'-1',
+            'error'=>true,
+            'name' => 'Datenbankfehler '.$ex->getCode(),
+            'beschreibung' => $ex->getMessage());
+    }
+    finally {
+        return $data;
+    }
+
+
+}
