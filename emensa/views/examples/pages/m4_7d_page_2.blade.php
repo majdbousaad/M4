abfe26b4dcd3e7bd2d4fd164dcda210fdcab5b59
majd bousaad <bousaadmajd@gmail.com>
2022-11-30 02:08:27 +0100

    Aufgabe 7 d) bootstrap layout
    link: https://www.w3schools.com/bootstrap/bootstrap_templates.asp

--- emensa/views/examples/pages/m4_7d_page_2.blade.php
+++ emensa/views/examples/pages/m4_7d_page_2.blade.php
@@ -1,1 +1,9 @@
-2
+@extends("examples.layout.m4_7d_layout")
+
+@section("main")
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda commodi dicta est ipsum maiores nam quos tempore tenetur? Aperiam architecto asperiores autem blanditiis consequatur exercitationem id minus quos, repellendus unde?</p>
+@endsection
+
+@section("footer")
+    footer page 2
+@endsection
